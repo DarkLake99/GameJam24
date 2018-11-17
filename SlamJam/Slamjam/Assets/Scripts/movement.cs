3146aabf554e9cbfb75a7000af49de4cdaabec02
Britney Khuu <britneykhuu@gmail.com>
2018-11-16 23:59:08 -0800

    added jump and movement to players 1 and 2

--- SlamJam/Slamjam/Assets/Scripts/movement.cs
+++ SlamJam/Slamjam/Assets/Scripts/movement.cs
@@ -6,16 +6,15 @@ public class movement : MonoBehaviour
 {
     bool onGround = true;
     public float speed = 5f;
-    public float jump = 5f;
+    public Vector2 jump;
+    public Rigidbody2D active;
     // Use this for initialization
 
     // Update is called once per frame
     void Update()
     {
         PlayerOne();
-        PlayerTwo();
-        float input = Input.GetAxis("Horizontal");
-        transform.position = transform.position + new Vector3(input * speed * Time.deltaTime, 0, 0);
+
     }
     void OnCollisionEnter2D(Collision2D variable)
     {
@@ -23,19 +22,28 @@ public class movement : MonoBehaviour
         {
             onGround = true;
         }
+       
     }
     void PlayerOne()
     {
         if(gameObject.tag=="Player 1")
         {
+            if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.RightArrow)))
+            {
+                float input = Input.GetAxis("Horizontal");
+                transform.position = transform.position + new Vector3(input * speed * Time.deltaTime, 0, 0);
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    if(onGround)
+                    {
+                        active.AddForce(jump, ForceMode2D.Impulse);
+                        onGround = false;
+                    }
+                   
+                }
 
+            }
         }
     }
-    void PlayerTwo()
-    {
-        if(gameObject.tag=="Player 2")
-        {
-
-        }
-    }
+  
 }
---
+++ SlamJam/Slamjam/Assets/Scripts/movement2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement2 : MonoBehaviour {
+
+    // Use this for initialization
+    bool onGround = true;
+    public float speed = 5f;
+    public Vector2 jump;
+    public Rigidbody2D active2;
+
+    // Update is called once per frame
+    void Update () 
+    {
+        PlayerTwo();
+    }
+    void OnCollisionEnter2D(Collision2D variable)
+    {
+        if (variable.gameObject.tag == "ground")
+        {
+            onGround = true;
+        }
+    }
+    void PlayerTwo()
+    {
+        if (gameObject.tag == "Player 2")
+        {
+            if (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
+            {
+                float move = Input.GetAxis("Horizontal-2");
+                transform.position = transform.position + new Vector3(move * speed * Time.deltaTime, 0, 0);
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    if (onGround)
+                    {
+                        active2.AddForce(jump, ForceMode2D.Impulse);
+                        onGround = false;
+                    }
+
+                }
+            }
+        }
+    }
+}
