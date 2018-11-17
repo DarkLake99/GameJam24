490e0e14265a1bb6a4125e609591d7d49120243e
Britney Khuu <britneykhuu@gmail.com>
2018-11-16 19:54:09 -0800

    made movements; need to edit tho edits on my scene

---
+++ SlamJam/Slamjam/Assets/Scripts/movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    bool onGround = true;
+    public float speed = 5f;
+    public float jump = 5f;
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerOne();
+        PlayerTwo();
+        float input = Input.GetAxis("Horizontal");
+        transform.position = transform.position + new Vector3(input * speed * Time.deltaTime, 0, 0);
+    }
+    void OnCollisionEnter2D(Collision2D variable)
+    {
+        if (variable.gameObject.tag == "ground")
+        {
+            onGround = true;
+        }
+    }
+    void PlayerOne()
+    {
+        if(gameObject.tag=="Player 1")
+        {
+
+        }
+    }
+    void PlayerTwo()
+    {
+        if(gameObject.tag=="Player 2")
+        {
+
+        }
+    }
+}
